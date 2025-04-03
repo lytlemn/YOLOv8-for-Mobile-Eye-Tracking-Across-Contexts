bab871c98d4f78dfa9c6b6f479c8778da6d39b22
JediyhIrony <129906836+JediyhIrony@users.noreply.github.com>
2025-04-03 15:53:01 -0400

    Update run_BFFs_MET.py

--- Val_Paper/MET_processing/run_BFFs_MET.py
+++ Val_Paper/MET_processing/run_BFFs_MET.py
@@ -3,7 +3,7 @@ from bffs_v8 import bffs_pipe
 import warnings
 parser = argparse.ArgumentParser()
 
-# -sub SUBJECT -vid VIDEO
+
 parser.add_argument("-s", "--subject", dest="subject", help="Subject Number")
 parser.add_argument("-v", "--video", action='store_true', help="Generate Video?")
 
@@ -12,12 +12,6 @@ subject = args.subject
 vidgen = args.video
 
 def main():
-    #If processing on file at a time uncomment below and comment out the many at once section
-    # global subject, vidgen, picgen
-    # message = ("Proceed with processing data for subject " + str(subject) + " [y/n] \n")
-    # if input(message) == "y":
-    #     warnings.simplefilter("ignore")
-    #     bffs_pipe(subject, vidgen)
     subjects = ["","",""] #subject numbers to process many at once
     for subject in subjects:
         warnings.simplefilter("ignore")
